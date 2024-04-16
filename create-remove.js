a6eaa8ccd63769901c387db8b0ef336adfd18193
Scott Feichter <scottfeichter@gmail.com>
2024-04-16 10:44:01 -0700

    good one to review

--- create-remove.js
+++ create-remove.js
@@ -1,44 +1,72 @@
 /****************************** ADD DOG BUTTON ******************************/
 const add = document.getElementById("add");
+const galleryList = document.querySelector(`.gallery > ul`);
+
 add.addEventListener("click", async () => {
-    try {
-        const res = await fetch("https://dog.ceo/api/breeds/image/random")
-        const data = await res.json();
+  try {
+    const res = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = await res.json();
 
-        const url = data.message; // URL of new dog image
+    const url = data.message; // URL of new dog image
 
-        /*--------------- Get breed (Hint: Parse from URL) ---------------- */
-        // Your code here 
+    /*--------------- Get breed (Hint: Parse from URL) ---------------- */
+    // Your code here
+    const dogName = url.split("/")[4];
+    console.log(dogName);
 
-        /*------------ Create new dog card with the url above ------------- */
-        /* (use the HTML structure for the current dog image in the index.html
+    /*------------ Create new dog card with the url above ------------- */
+    /* (use the HTML structure for the current dog image in the index.html
             file to create a new image with the url) */
-        // Your code here 
+    // Your code here
+    const li = document.createElement("li");
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const figCaption = document.createElement("figCaption");
 
-        /* Add the new dog card as a child to the ul in the .gallery element */
-        // Your code here 
+    li.appendChild(figure);
+    figure.appendChild(img);
+    figure.appendChild(figCaption);
 
-    } catch (e) {
-        console.log("Couldn't fetch dog :(")
-    }
+    console.log(li);
+
+    img.src = url;
+    figCaption.innerText = dogName;
+    console.log(li);
+
+    /* Add the new dog card as a child to the ul in the .gallery element */
+    // Your code here
+    galleryList.appendChild(li);
+  } catch (e) {
+    console.log("Couldn't fetch dog :(");
+    console.log(e);
+  }
 });
 
 /************************** REMOVE FIRST DOG BUTTON **************************/
 const removeFirst = document.getElementById("remove-first");
 removeFirst.addEventListener("click", () => {
-    /*-------------------- Select the first dog card --------------------- */
-    // Your code here 
+  /*-------------------- Select the first dog card --------------------- */
+  // Your code here
+  const firstListChild = document.querySelector("li");
 
-    /*-------------------- Remove the first dog card --------------------- */
-    // Your code here 
+  /*-------------------- Remove the first dog card --------------------- */
+  // Your code here
+  firstListChild
+  ? firstListChild.remove()
+  : console.log(`no more dogs to remove`);
+
 });
 
 /************************** REMOVE LAST DOG BUTTON ***************************/
 const removeLast = document.getElementById("remove-last");
 removeLast.addEventListener("click", () => {
-    /*-------------------- Select the last dog card ----------------------- */
-    // Your code here 
+  /*-------------------- Select the last dog card ----------------------- */
+  // Your code here
+  const lastListChild = document.querySelectorAll("li")[length-1];
+  /*-------------------- Remove the last dog card ----------------------- */
+  // Your code here
+  lastListChild
+  ? lastListChild.remove()
+  : console.log(`no more dogs to remove`);
 
-    /*-------------------- Remove the last dog card ----------------------- */
-    // Your code here 
 });
